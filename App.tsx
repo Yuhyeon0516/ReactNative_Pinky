cb73c7c29b9b6b6f2afac5ea9398c3d23d2d646d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-06 21:47:34 +0900

    Created tabbar & base of screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,115 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import React from "react";
+import HomeScreen from "./src/screens/Home";
+import {NavigationContainer} from "@react-navigation/native";
+import GroupScreen from "./src/screens/Group";
+import SettingScreen from "./src/screens/Setting";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import {Text} from "react-native";
+
+type TabList = {
+    Home: undefined;
+    Setting: undefined;
+    Group: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabList>();
 
 function App(): JSX.Element {
-    return <SafeAreaView />;
+    return (
+        <NavigationContainer>
+            <SafeAreaProvider>
+                <Tab.Navigator
+                    screenOptions={({route}) => {
+                        return {
+                            headerShown: false,
+                            tabBarActiveTintColor: "#e8aced",
+                            tabBarInactiveTintColor: "black",
+                            tabBarStyle: {
+                                paddingTop: 7,
+                                borderTopLeftRadius: 24,
+                                borderTopRightRadius: 24,
+                                borderLeftWidth: 0.2,
+                                borderRightWidth: 0.2,
+                                position: "absolute",
+                                overflow: "hidden",
+                                minHeight: 60,
+                            },
+                            // eslint-disable-next-line react/no-unstable-nested-components
+                            tabBarIcon({color, size}) {
+                                if (route.name === "Home") {
+                                    return (
+                                        <MaterialIcons
+                                            name="home"
+                                            color={color}
+                                            size={size}
+                                        />
+                                    );
+                                } else if (route.name === "Group") {
+                                    return (
+                                        <MaterialIcons
+                                            name="groups"
+                                            color={color}
+                                            size={size}
+                                        />
+                                    );
+                                } else {
+                                    return (
+                                        <MaterialIcons
+                                            name="settings"
+                                            color={color}
+                                            size={size}
+                                        />
+                                    );
+                                }
+                            },
+                            // eslint-disable-next-line react/no-unstable-nested-components
+                            tabBarLabel({color}) {
+                                if (route.name === "Home") {
+                                    return (
+                                        <Text
+                                            style={{
+                                                color: color,
+                                                fontSize: 14,
+                                                fontWeight: "600",
+                                            }}>
+                                            홈
+                                        </Text>
+                                    );
+                                } else if (route.name === "Group") {
+                                    return (
+                                        <Text
+                                            style={{
+                                                color: color,
+                                                fontSize: 14,
+                                                fontWeight: "600",
+                                            }}>
+                                            그룹
+                                        </Text>
+                                    );
+                                } else {
+                                    return (
+                                        <Text
+                                            style={{
+                                                color: color,
+                                                fontSize: 14,
+                                                fontWeight: "600",
+                                            }}>
+                                            설정
+                                        </Text>
+                                    );
+                                }
+                            },
+                        };
+                    }}>
+                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name="Group" component={GroupScreen} />
+                    <Tab.Screen name="Setting" component={SettingScreen} />
+                </Tab.Navigator>
+            </SafeAreaProvider>
+        </NavigationContainer>
+    );
 }
 
 export default App;
---
+++ src/screens/Setting.tsx
@@ -0,0 +1,10 @@
+import {Text, SafeAreaView} from "react-native";
+import React from "react";
+
+export default function SettingScreen() {
+    return (
+        <SafeAreaView>
+            <Text>Setting</Text>
+        </SafeAreaView>
+    );
+}
